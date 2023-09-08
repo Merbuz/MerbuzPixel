195ae730330bbfa265f64096f715e9bffd05934e
Merbuz <75749391+Merbuz@users.noreply.github.com>
2023-09-08 21:54:52 +0300

    Update merbuzpixel.py
    version 0.2 update

--- merbuzpixel.py
+++ merbuzpixel.py
@@ -2,14 +2,30 @@ import mip # Import mip for install neopixel
 
 mip.install('neopixel')
 
-import neopixel 
+import neopixel
+from machine import Pin
 
 class MerbuzPixel:
-    def __init__(self, pin, leds):
-        self.pin = pin # pin for the connect led
-        self.leds = leds # count of leds
-        self.brightness = 1 # default brightness
-        
+    # Default colors
+
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    ORANGE = (255, 128, 0)
+    YELLOW = (255, 255, 0)
+    LIGHTBLUE = (0, 255, 255)
+    WHITE = (255, 255, 255)
+    VIOLET = (127, 0, 255)
+    PURPLE = (255, 0, 255)
+    PINK = (255, 0, 127)
+    
+    def __init__(self, pin, width, height):
+        self.pin = Pin(pin, Pin.OUT) # pin for the connect led
+        self.width = width # width of matrix
+        self.height = height # height of matrix
+
+        self.brightness = 1.0 # default brightness
+        self.leds = self.width * self.height 
         self.np = neopixel.NeoPixel(self.pin, self.leds, 3)
 
     def setBrightness(self, brightness):
@@ -39,18 +55,22 @@ class MerbuzPixel:
     
     def fillBackground(self, rgb):
         # Fill all matrix of selected color
+
         for i in range(self.leds):
             self.np[i] = (int(rgb[0] * self.brightness), int(rgb[1] * self.brightness), int(rgb[2] * self.brightness))
         self.np.write()
     
     def clearMatrix(self):
-        # Send text 
+        # Clear all matrix
+
         for i in range(self.leds):
             self.np[i] = (0,0,0)
         self.np.write()
     
     def clearPixel(self, index):
+        # Clear current pixel
+
         if index > self.leds:
-            raise Exception('Index must be more or less than count of leds')
+            raise Exception('Index more than count of leds')
         self.np[index] = (0,0,0)
         self.np.write()
